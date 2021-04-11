34ef9d93cc9f8f8badb4045c183d117a654e2e6b
fafaovo <aa1206180803@sina.cn>
2021-04-11 12:10:32 +0800

    1

--- 今日头条/pages/index4/index4.js
+++ 今日头条/pages/index4/index4.js
@@ -6,9 +6,44 @@ Page({
    */
   data: {
     tab:"0",
+    array:[
+      {message:'foo'},
+      {message:'bar'}
+  ],
+  array1:[
+    {
+      message:'啥时候开学！啥时候可以摘下口罩！啥时候可以出去玩',
+      img1:'/pages/images/img1.jpg',
+      img2:'/pages/images/img2.jpg',
+      img3:'/pages/images/img3.jpg',
+    },
+    {
+      message:'"中国制造别用" ？耿秋：别用！别穿！别戴',
+      img1:'/pages/images/img4.jpg',
+      img2:'/pages/images/img5.jpg',
+      img3:'/pages/images/img6.jpg',
+    },
+    {
+      message:'哪里来的回哪去！官方回应女子拒接隔离大闹机场辱骂防疫人员',
+      img1:'/pages/images/img7.jpg',
+      img2:'/pages/images/img8.jpg',
+      img3:'/pages/images/img9.jpg',
+    },
+    {
+      message:'疫情速报：特朗普直呼内行！拜登在飞机登机口连连下跪！',
+      img1:'/pages/images/img10.jpg',
+      img2:'/pages/images/img11.jpg',
+      img3:'/pages/images/img12.jpg',
+    },
+  ]
+
   },
   tab:function(e){
     var a = e.target.dataset.num;
+    this.setData({tab:a});
+  },
+  change:function(e){
+    var a = e.detail.current;
     this.setData({tab:a});
   },
   /**
